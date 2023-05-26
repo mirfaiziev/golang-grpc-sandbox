60ed4b82489ac8b5667e184b436e6d2f860de099
mirfaiziev <mirfaiziev.victor@gmail.com>
2023-05-26 18:25:04 +0200

    implement grpc server

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"net"
+	"log"
+	"google.golang.org/grpc"
+	"context"
+	pb "github.com/mirfaiziev/golang-grpc-sandbox/server/internal/pb"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHelloWorld(ctx context.Context, in *emptypb.Empty) (*pb.HelloWorldReply, error) {
+	return &pb.HelloWorldReply{Message: "Hello word"}, nil
+}
 
 func main() {
-	fmt.Println("hello word")
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
